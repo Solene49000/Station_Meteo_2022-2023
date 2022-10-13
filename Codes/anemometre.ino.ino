3a3e7c610d047b662dee2af47b9be33febd06136
ambrinou <90826762+ambrinou@users.noreply.github.com>
2022-10-13 11:44:14 +0200

    Add files via upload

--- Codes/anemometre.ino.ino
+++ Codes/anemometre.ino.ino
@@ -1,38 +1,50 @@
 #include <ESP32TimerInterrupt.h>
 #include <ESP32TimerInterrupt.hpp>
 
-float vitesseVent = 0;
-int anemometre=27 ; 
-int tourAnemo=0 ;
-int instant =0;
-int tempsAnemo =1;
-int debutTempsAnemo =0;
+#define ANEMOMETRE 27
 #define MEASURE 13
+#define LED_R 33
+#define RAYON 0,07 // rayon de l'anemometre de 7 cm
 
-void setup()
-{
-pinMode(MEASURE, OUTPUT);
-digitalWrite(MEASURE, HIGH);
-Serial.begin(115200) ;
-attachInterrupt(anemometre,comptageAnemo,FALLING) ; 
-}
-void loop()
-{
-  Serial.println("Nombre de 1/2 tour : " ) ;
+float vitesseVent = 0;
+int tourAnemo=0 ;
+int tempsTimer = 10 ;
+
+hw_timer_t *My_timer = NULL;
+
+void IRAM_ATTR onTimer(){
+  Serial.println("-------------------------------------------------------------------") ;
+  Serial.print("Nombre de 1/2 tour : " ) ;
   Serial.println(tourAnemo) ;
-  cycleAnemometre() ;
-  delay(1000);
+  //3.6 pour passer de m/s en km/h
+  vitesseVent = (PI * RAYON * 2 * tourAnemo)/tempsTimer*3.6 ;
+  Serial.print("Vitesse du vent : (km/h)") ;
+  Serial.println(String(vitesseVent, 2)) ;
+  Serial.println("-------------------------------------------------------------------") ;
+  tourAnemo = 0 ;
 }
+
+void setup() {
+ 
+  Serial.begin(115200);
+  pinMode(LED_R, OUTPUT);
+  pinMode (ANEMOMETRE, INPUT);
+  pinMode(MEASURE, OUTPUT);
+  digitalWrite(MEASURE, HIGH);
+
+  My_timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(My_timer, &onTimer, true);
+  timerAlarmWrite(My_timer, tempsTimer*1000000, true); // microsecondes
+  timerAlarmEnable(My_timer);
+  
+  attachInterrupt(ANEMOMETRE,comptageAnemo,FALLING) ;
+}
+
+void loop()
+{  
+}
+
 void comptageAnemo()
 {
   tourAnemo ++ ;
-}
-
-void cycleAnemometre(){
-  instant = millis();
-  debutTempsAnemo = instant;
-  vitesseVent = tourAnemo*2.4;
-  Serial.println("Vitesse du vent : (km/h)") ;
-  Serial.println(String(vitesseVent, 2)) ;
-  //return vitesseVent;
 }
