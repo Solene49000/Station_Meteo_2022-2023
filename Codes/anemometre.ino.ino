1bec040e7f37963c6391f2fd38764fa81d1ddbd2
ambrinou <90826762+ambrinou@users.noreply.github.com>
2022-12-09 16:25:02 +0100

    Add files via upload

--- Codes/anemometre.ino.ino
+++ Codes/anemometre.ino.ino
@@ -4,46 +4,48 @@
 #define ANEMOMETRE 27
 #define MEASURE 13
 #define LED_R 33
-#define RAYON 0,07 // rayon de l'anemometre de 7 cm
+#define RAYON 0.07 // rayon de l'anemometre de 7 cm
+#define B1000000 64
+#define B110 6
 
-float vitesseVent = 0;
+float vitesseVent;
 int tourAnemo=0 ;
-int tempsTimer = 10 ;
+unsigned long startTime_A;  
+unsigned long currentTime_A;
+const unsigned long period_A = 10000;
 
-hw_timer_t *My_timer = NULL;
-
-void IRAM_ATTR onTimer(){
-  Serial.println("-------------------------------------------------------------------") ;
-  Serial.print("Nombre de 1/2 tour : " ) ;
-  Serial.println(tourAnemo) ;
-  //3.6 pour passer de m/s en km/h
-  vitesseVent = (PI * RAYON * 2 * tourAnemo)/tempsTimer*3.6 ;
-  Serial.print("Vitesse du vent : (km/h)") ;
-  Serial.println(String(vitesseVent, 2)) ;
-  Serial.println("-------------------------------------------------------------------") ;
-  tourAnemo = 0 ;
-}
+void comptageAnemo();
+void Anemo();
 
 void setup() {
- 
   Serial.begin(115200);
+  Serial.println("test");
   pinMode(LED_R, OUTPUT);
   pinMode (ANEMOMETRE, INPUT);
   pinMode(MEASURE, OUTPUT);
   digitalWrite(MEASURE, HIGH);
-
-  My_timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(My_timer, &onTimer, true);
-  timerAlarmWrite(My_timer, tempsTimer*1000000, true); // microsecondes
-  timerAlarmEnable(My_timer);
-  
-  attachInterrupt(ANEMOMETRE,comptageAnemo,FALLING) ;
+  attachInterrupt(ANEMOMETRE,comptageAnemo,FALLING) ; 
 }
-
+ 
 void loop()
-{  
+{
+  currentTime_A = millis();
+  if (currentTime_A - startTime_A >= period_A)//test whether the period has elapsed
+  { 
+    Anemo();
+    startTime_A = currentTime_A ;
+  }
 }
 
+void Anemo(){
+  Serial.print("Nombre de 1/2 tour : " ) ;
+  Serial.println(tourAnemo) ;
+  //3.6 pour passer de m/s en km/h
+  vitesseVent = ((PI*RAYON*2*tourAnemo)/period_A)*3.6*1000 ;
+  Serial.print("Vitesse du vent : (km/h) ") ;
+  Serial.println(String(vitesseVent, 2)) ;
+  tourAnemo = 0 ;
+}
 void comptageAnemo()
 {
   tourAnemo ++ ;
